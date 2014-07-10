cca226251d9bbcd01ec4909436abadcbdee18ea9
Kromosbg <ivo383@abv.bg>
2014-07-10 13:15:51 +0300

    Create Cd.java
    opens directory.The same as cd in command prompt

---
+++ Cd.java
@@ -0,0 +1,13 @@
+package stringvars;
+
+import java.io.File;
+
+public class Cd {
+	
+	String newFolder="";
+	
+	public  File  runCd() {
+		File resultFolder=new File(newFolder);
+		return resultFolder;
+	}
+}
