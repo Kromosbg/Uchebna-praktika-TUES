0e89c90e5040b0c6cf08ec47badb927dcce66614
Kromosbg <ivo383@abv.bg>
2014-07-11 15:22:11 +0300

    Update Cd.java
    Cd  and cd..

--- Cd.java
+++ Cd.java
@@ -2,12 +2,37 @@ package stringvars;
 
 import java.io.File;
 
-public class Cd {
+public class Cd implements Command {
+
+	private String newFolder = "";
 	
-	String newFolder="";
-	
-	public  File  runCd() {
-		File resultFolder=new File(newFolder);
-		return resultFolder;
+
+	public Cd(String folder) {
+		newFolder = folder;
+	}
+
+	public String getNewFolder() {
+		return newFolder;
+	}
+
+	public void setNewFolder(String newFolder) {
+		this.newFolder = newFolder;
+	}
+
+	public File executeCommand(File currentFolder) {
+
+		if ("..".equals(newFolder)) {
+			File resultFolder = new File(currentFolder.getParent());
+			return resultFolder;
+		} else {
+			File resultFolder = new File(currentFolder.getAbsolutePath() + "\\"
+					+ newFolder);
+			if (resultFolder.exists()) {
+				return resultFolder;
+			} else {
+				System.out.println("The system cannot find the path specified");
+			}
+			return currentFolder;
+		}
 	}
 }
