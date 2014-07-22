37e0b2708903a9c7d258955f4d20ce2cb433ce3a
Kromosbg <ivo383@abv.bg>
2014-07-22 15:32:05 +0300

    Update Echo.java

--- Echo.java
+++ Echo.java
@@ -1,10 +1,30 @@
-package stringvars;
+package stringvars.commands;
 
-public class Echo {
-	String message;
+import java.io.File;
+
+import stringvars.Command;
+
+
+public class Echo implements Command{
 	
-	public void executeCommand(){
-		
-		System.out.println(message);
+	public String message;
+	
+
+	public Echo(String echo) {
+		echo=message;
 	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public File executeCommand(File currentFolder) {
+		System.out.println(message);	
+		return currentFolder;
+	}
+
 }
