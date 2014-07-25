4743568d19bf63c47321d8f0a33aa57feb0833c9
Kromosbg <ivo383@abv.bg>
2014-07-25 16:21:54 +0300

    Update Cls.java

--- Promp/Cls.java
+++ Promp/Cls.java
@@ -2,14 +2,20 @@ package stringvars.commands;
 
 import java.io.File;
 
+import stringvars.ComResponse;
 import stringvars.Command;
 
 public class Cls implements Command{
-
-	public File executeCommand(File currentFolder) {
-		for( int i = 0; i < 25; i++ ) {
-		    System.out.println("");
+	
+	
+	public ComResponse executeCommand(File resultFolder) {
+		StringBuilder text = new StringBuilder("");
+		for( int i = 0; i < 100; i++ ) {
+			text.append("\n");
 		}
-		return currentFolder;
+	
+	
+		ComResponse sb= new ComResponse(resultFolder, text);
+		return sb;
+		}
 	}
-}
