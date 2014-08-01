09d20a5c1eb7e018ab2504f19650e35f89b5d938
Kromosbg <ivo383@abv.bg>
2014-08-01 14:33:01 +0300

    Update CommandManager.java

--- commands/CommandManager.java
+++ commands/CommandManager.java
@@ -2,48 +2,67 @@ package stringvars;
 
 import stringvars.commands.Cd;
 import stringvars.commands.Cls;
+import stringvars.commands.Copy;
+import stringvars.commands.Delete;
 import stringvars.commands.Dir;
 import stringvars.commands.Echo;
 import stringvars.commands.Md;
+import stringvars.commands.Move;
 import stringvars.commands.Rd;
 
 public class CommandManager {
-	public Command getCommand(String cmd, String argument) throws BadCommandException {
+	public Command getCommand(String cmd, String argument)
+			throws BadCommandException {
 
+		if (cmd.equals("echo")) {
+			Echo echo = new Echo(argument);
+			return echo;
+		}
 
-		        
-	if(cmd.equals("echo")) {
-		Echo echo=new Echo(argument);
-		return echo;
-	}
-	
-	if(cmd.equals("dir")) {
-		Dir newDir = new Dir();
-		return newDir;
-	}
-	
-	if(cmd.equals("cd")) {
-		Cd newCd = new Cd(argument);				
-		return newCd;
-	}
-
-
-	if(cmd.equals("md") || cmd.equals("mkdir")) {
-		Md newMd = new Md(argument);
-		return newMd;
-	}
-
-	
-	if(cmd.equals("rd") || cmd.equals("rmdir")) {
-		Rd newRd = new Rd(argument);
-		return newRd;
-	}
-	if(cmd.equals("cls")){
-		Cls newCls=new Cls();
-		return newCls;
+		if (cmd.equals("move")){
+			Move move = new Move(argument);
+			return move;
+		}
 		
-	}
+		if (cmd.equals("dir")) {
+			Dir newDir = new Dir();
+			return newDir;
+		}
 
-    throw new BadCommandException();
+		if (cmd.equals("delete")) {
+			Delete newDelete = new Delete(argument);
+			return newDelete;
+		}
+
+		if (cmd.equals("copy")) {
+			Copy newCopy = new Copy(argument);
+			return newCopy;
+		}
+
+		if (cmd.equals("cd")) {
+			Cd newCd = new Cd(argument);
+			return newCd;
+		}
+		if (cmd.equals("cd..")) {
+			Cd newCd = new Cd("..");
+			return newCd;
+		}
+
+		if (cmd.equals("md") || cmd.equals("mkdir")) {
+			Md newMd = new Md(argument);
+			return newMd;
+		}
+
+		if (cmd.equals("rd") || cmd.equals("rmdir")) {
+			Rd newRd = new Rd(argument);
+			return newRd;
+		}
+		if (cmd.equals("cls")) {
+			Cls newCls = new Cls();
+			return newCls;
+
+		}
+
+		throw new BadCommandException();
 	}
 }
