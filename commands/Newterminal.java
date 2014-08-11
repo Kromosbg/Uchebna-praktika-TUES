02b0dd03a9925014d5f306950b29bc49421b0ed5
Kromosbg <ivo383@abv.bg>
2014-08-11 14:38:59 +0300

    Update Newterminal.java

--- commands/Newterminal.java
+++ commands/Newterminal.java
@@ -1,36 +1,64 @@
 package stringvars;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import com.xentio.terminal.protocol.Protocol;
 
-public class Newterminal{
-	                             
-		public void executeCommand() throws Exception {
- 
-		
-			String cmd="";
-			String workingDir =  System.getProperty("user.dir"); 
+import stringvars.commands.Command;
+
+public class Newterminal {
+
+	public void run(String[] args) {
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(5000);
+
+			Socket clientSocket = serverSocket.accept();
+			
+			
+			PrintStream outputStream =  new PrintStream(clientSocket.getOutputStream()); //System.out;
+			InputStream inputStream = clientSocket.getInputStream();//System.in;
+
+			Scanner userInput = new Scanner(inputStream);
+
+			String commandName = "";
+			String workingDir = System.getProperty("user.dir");
 			File currentFolder = new File(workingDir);
-			CommandManager manager=new CommandManager();
-			
-			while (!cmd.equals("exit")) {	
-			System.out.print( currentFolder.getAbsolutePath() + "> ");
-		
-			Scanner userInput = new Scanner(System.in);
-			cmd = userInput.next();
-			
-			String argument = userInput.nextLine().trim();
-			try {
-			Command command=manager.getCommand(cmd,argument);
-			currentFolder=command.executeCommand(currentFolder);	
-			} catch (BadCommandException e) {
-				System.out.println(cmd);
-				System.out.print("  is not recognized as an internal or external command,operable program or batch file");
-				System.out.println( currentFolder.getAbsolutePath() + "> ");
+			CommandManager commandManager = new CommandManager();
 
-			}		
-			
+			while (!commandName.equals("shutdown")) {
+				
+				outputStream.println(currentFolder.getAbsolutePath() + "> ");
+				outputStream.println(Protocol.END_OF_COMMAND_STRING);
+		
+				commandName = userInput.next();
+		
+				
+				String argument = userInput.nextLine().trim();
+				try {
+					Command command = commandManager.getCommand(commandName,argument);
+
+					ComResponse response = command.executeCommand(currentFolder);
+					currentFolder = response.getCurrentfolder();
+					outputStream.println(response.getText());
+				} catch (BadCommandException e) {
+					outputStream.println(commandName);
+					outputStream.print("  is not recognized as an internal or external command,operable program or batch file");
+					outputStream.println(currentFolder.getAbsolutePath() + "> ");
+				}
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 	}
+
 }
