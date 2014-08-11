db12eae0a3d51279f7b05ae6e395767f5c970003
Kromosbg <ivo383@abv.bg>
2014-08-11 14:36:59 +0300

    Update Rd.java

--- Promp/Rd.java
+++ Promp/Rd.java
@@ -2,39 +2,36 @@ package stringvars.commands;
 
 import java.io.File;
 
-import stringvars.Command;
+import stringvars.ComResponse;
 
-public class Rd implements Command{
-	public String newFolder = "";
-	
-	public Rd(String argument) {
-		newFolder = argument;
+public class Rd extends CommandWithArgument implements Command{
+
+	public Rd(String newString) {
+		super(newString);
 	}
 
-	public String getNewFolder() {
-		return newFolder;
-	}
 
-	public void setNewFolder(String newFolder) {
-		this.newFolder = newFolder;
-	}
-		
-	public File executeCommand(File currentFolder) {
-	
-		 File newDir = new File(currentFolder.getAbsolutePath() + "\\"
-					+ newFolder);
-		 
-		  if (newDir.exists()) {
-			  try {		
 
-		 newDir.delete();
-		 System.out.println("File deleted successfully");
-		}  catch(SecurityException se){
-			 System.out.println("No such file");
+	public ComResponse executeCommand(File currentFolder) {
+			StringBuilder text = new StringBuilder();
 			
-			}
-		}
-		return  currentFolder;
-	 }
-}
+		 File newDir = new File(currentFolder.getAbsolutePath() + "\\" + toString());
+			
 
+		  if (newDir.exists()) {
+				
+		 newDir.delete();
+		 
+		 text.append("File deleted successfully");
+		  }   else {
+	
+
+			 text.append("No such file"); 
+		  
+		}
+		
+		ComResponse sb= new ComResponse(currentFolder , text);
+		return sb;
+		  
+}
+}
